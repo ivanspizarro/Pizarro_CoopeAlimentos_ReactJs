dad9b97631059ac281e16e691725ad754d3a64d3
ivanspizarro <100243203+ivanspizarro@users.noreply.github.com>
2022-03-11 15:08:56 -0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+import Navv from "./Navv"
+
+const Footer = () => {
+  return (
+    <footer>Soy el Footer eee
+
+<Navv></Navv>
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,82 @@
+import Counter from "./Counter";
+import { useState } from "react";
+import { useEffect } from "react";
+
+let productos = [
+{
+
+  id: 1,
+
+  nombre: "Jamon",
+
+precio: 100,
+},
+
+{
+
+  id: 2,
+
+  nombre: "QUeso",
+
+precio: 100,
+},
+
+{
+
+  id: 1,
+
+  nombre: "Salame",
+
+precio: 100,
+}
+
+
+
+
+
+]
+
+const ItemListContainer = (props) => {
+
+  const [loading, setLoading] = useState(true);
+
+  const [products, setProducts] = useState(["jabon", "Lisoform"]);
+
+  useEffect ( ()=> {
+setTimeout(() => {
+
+  setProducts(products);
+
+  console.log(products);
+
+  setLoading (false)
+  
+}, 3000);
+
+console.log (productos)
+  }
+
+
+
+  )
+  
+    return (
+     
+    <main> Bienvenido Heroe de Trafalgar {props.name} {props.surname} 
+
+    <Counter> </Counter>
+
+    <h2> {loading ? "Cargando, espera salame" : productos.map ((a,b) => {
+
+return <li>{productos.nombre}</li>
+
+
+
+      } ) } </h2>
+    
+    
+    </main> )
+ } 
+
+
+export default ItemListContainer
